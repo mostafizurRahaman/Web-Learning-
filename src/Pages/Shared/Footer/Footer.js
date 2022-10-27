000c2a2fd72b0a8c3f1d4f96229f80b5d88a976e
mostafizurRahaman <fahim654326@gmail.com>
2022-10-27 18:59:06 +0600

    vercel link is added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
    return (
-      <div className='w-full py-5 bg-violet-600 border-t-2 border-t-white'>
+      <div className='w-full py-5 bg-violet-600 dark:bg-gray-800 border-t-2 border-t-white'>
          <h3 className='text-white font-semibold  text-base text-center'>All &copy;right reserved by Web learning</h3>
       </div>
    );
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -9,50 +9,67 @@ import Login from "../../Pages/Login/Login";
 import Profile from "../../Pages/Profile/Profile";
 import Register from "../../Pages/Register/Register";
 import PrivateRoute from "../PrivateRoute/PrivateRoute";
-import Courses from './../../Pages/Courses/Courses';
+import Courses from "./../../Pages/Courses/Courses";
 
 const routes = createBrowserRouter([
-   {path: '/', element: <Main></Main>, children: [
-      {path: '/' , element: <Home></Home>}, 
-      {path: '/home', element: <Home></Home>},
-      {
-      path:'/courses', 
-      element: <Courses></Courses>,
-      loader: async()=> fetch('http://localhost:5000/courses')
-      }, 
-      {
-         path:'/courses/:id', 
-         element: <CourseDetails></CourseDetails>, 
-         loader: async({params}) => fetch(`http://localhost:5000/courses/${params.id}`)
-      }, 
-      {
-         path:'/checkout/:id', 
-         element: <PrivateRoute><CheackOut></CheackOut> </PrivateRoute>, 
-         loader: async({params}) => fetch(`http://localhost:5000/courses/${params.id}`)
-      },
-      {
-         path:'/register', 
-         element: <Register></Register>
-      },
-      {
-         path:'/login', 
-         element: <Login></Login>
-      },   
-      {
-         path: '/profile', 
-         element: <PrivateRoute><Profile></Profile> </PrivateRoute>
-      },
-      {
-         path: '/blog', 
-         element: <Blog></Blog>
-      },
-      {
-         path: '*', 
-         element: <Error></Error>
-      }
+   {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+         { path: "/", element: <Home></Home> },
+         { path: "/home", element: <Home></Home> },
+         {
+            path: "/courses",
+            element: <Courses></Courses>,
+            loader: async () =>
+               fetch("https://web-learning-server-six.vercel.app/courses"),
+         },
+         {
+            path: "/courses/:id",
+            element: <CourseDetails></CourseDetails>,
+            loader: async ({ params }) =>
+               fetch(
+                  `https://web-learning-server-six.vercel.app/courses/${params.id}`
+               ),
+         },
+         {
+            path: "/checkout/:id",
+            element: (
+               <PrivateRoute>
+                  <CheackOut></CheackOut>{" "}
+               </PrivateRoute>
+            ),
+            loader: async ({ params }) =>
+               fetch(
+                  `https://web-learning-server-six.vercel.app/courses/${params.id}`
+               ),
+         },
+         {
+            path: "/register",
+            element: <Register></Register>,
+         },
+         {
+            path: "/login",
+            element: <Login></Login>,
+         },
+         {
+            path: "/profile",
+            element: (
+               <PrivateRoute>
+                  <Profile></Profile>{" "}
+               </PrivateRoute>
+            ),
+         },
+         {
+            path: "/blog",
+            element: <Blog></Blog>,
+         },
+         {
+            path: "*",
+            element: <Error></Error>,
+         },
+      ],
+   },
+]);
 
-   ]}, 
-  
-])
-
-export default routes; 
+export default routes;
